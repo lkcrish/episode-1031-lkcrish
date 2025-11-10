b87c77265632a47b04e3fdb1c81aa00afc625ac8
Lauren <lkcrish@icloud.com>
2025-11-10 15:03:59 -0800

    verion 2 of pi estimator

--- pi_estimator.py
+++ pi_estimator.py
@@ -5,5 +5,7 @@ darts_in_circle = 0
 for i in range(total_num):
     dartx = random.random()
     darty = random.random()
-    print(f'dart{i} is at ({dartx}, {darty})')
+    if dartx*dartx + darty*darty <= 1:
+        darts_in_circle += 1
+    print(f'Our estimate for pi is {4.0* darts_in_circle /total_num})')
 
