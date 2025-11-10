b410a0abdf7de5bccf073b6b5a88a5a28daba0d3
Lauren <lkcrish@icloud.com>
2025-11-10 15:33:55 -0800

    fixed indent change

--- pi_estimator.py
+++ pi_estimator.py
@@ -7,9 +7,9 @@ darts_in_circle = 0
 for i in range(total_num):
    print("Throwing dart...") 
    dartx = random.random()
-    darty = random.random()
-    print (f'dart {i} is at ({dartx}, {darty})')
-    if dartx*dartx + darty*darty <= 1:
-        darts_in_circle += 1
-    print(f'Our estimate for pi is {4.0* darts_in_circle /total_num})')
+   darty = random.random()
+   print (f'dart {i} is at ({dartx}, {darty})')
+   if dartx*dartx + darty*darty <= 1:
+   	darts_in_circle += 1
+   print(f'Our estimate for pi is {4.0* darts_in_circle /total_num})')
 
