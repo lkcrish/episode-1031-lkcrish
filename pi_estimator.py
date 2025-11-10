9209c6129738e5e171c56d0dd890b8976fd1d8bd
Lauren <lkcrish@icloud.com>
2025-11-10 15:30:57 -0800

    adding a print

--- pi_estimator.py
+++ pi_estimator.py
@@ -5,7 +5,8 @@ import sys
 total_num = int(sys.argv[1])
 darts_in_circle = 0
 for i in range(total_num):
-    dartx = random.random()
+   print("Throwing dart...") 
+   dartx = random.random()
     darty = random.random()
     print (f'dart {i} is at ({dartx}, {darty})')
     if dartx*dartx + darty*darty <= 1:
