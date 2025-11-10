53b3d93fbb6f83c21540a866490350954a36956a
Lauren <lkcrish@icloud.com>
2025-11-10 15:01:04 -0800

    version 1 of pi estimator

--- pi_estimator.py
+++ pi_estimator.py
@@ -1,1 +1,9 @@
+mport random
+
+total_num - 10000
+darts_in_circle = 0
+for i in range(total_num):
+    dartx = random.random()
+    darty = random.random()
+    print(f'dart{i} is at ({dartx}, {darty})')
 
