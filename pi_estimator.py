4e62c9a8d66e2160107a0daf3ce4413a53d21418
Lauren <lkcrish@icloud.com>
2025-11-10 15:22:33 -0800

    included sys in the code, version 3 of pi estimator

--- pi_estimator.py
+++ pi_estimator.py
@@ -1,10 +1,13 @@
-import random
 
-total_num = 10000
+import random
+import sys
+
+total_num = int(sys.argv[1])
 darts_in_circle = 0
 for i in range(total_num):
     dartx = random.random()
     darty = random.random()
+    print (f'dart {i} is at ({dartx}, {darty})')
     if dartx*dartx + darty*darty <= 1:
         darts_in_circle += 1
     print(f'Our estimate for pi is {4.0* darts_in_circle /total_num})')
